944c3aa6a944b182b6f7636baf6006f0bfebf69b
JonWang <jiongwang1984@gmail.com>
2020-03-10 19:36:25 +0100

    gif_for_Likelihood

--- likelihoodFunc/bayesianRegression.py
+++ likelihoodFunc/bayesianRegression.py
@@ -1,39 +1,78 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  6 16:53:07 2020
+#-------------------------------------------------------------------------------
+# Name:        Toy Bayesian Inference for linear regression
 
-@author: wang0096
-"""
+##########
+# Main
+##########
+
+# Purpose:     
+#              1. for education and practice
+#              2. ...
+#              Main method is ...
+#
+# Modifications ~~:
+#              1.
+#
+# Author:      Jiong Wang
+#
+# Created:     10/03/2020
+# Copyright:   (c) JonWang 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
 
 import numpy as np
+import pylab as pl
+import matplotlib.pyplot as plt
+
 from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import matplotlib.pyplot as plt
 
-# Prior function
+# Prior function for a simple linear model with only a interception and a slope
 def pri(theta1,theta2):
-    func = (1/(np.sqrt(6.28)*3))*np.exp((theta1**2 + theta2**2)**2/(-2*3))
-    return func  #(1-(x**2+y**3))*exp(-(x**2+y**2)/2)
+    sigma = 3  # Standard deviation of the Gaussian prior
+    func = (1/np.sqrt(6.28*sigma**2))*np.exp((theta1**2 + theta2**2)**2/(-2*sigma**2))
+    return func
 
-# Likelihood function given one observation
-def likeli(theta1,theta2,obs_y,obs_x):
-    func = (1/np.sqrt(6.28))*np.exp((obs_y-theta1-theta2*obs_x)**2/(-2))
-    return func  #(1-(x**2+y**3))*exp(-(x**2+y**2)/2)
+# Likelihood function for one observation
+# For multiple observations only needs to multiply this function
+def likeli(theta1,theta2,obs_y,obs_x):  # It is a function of theta with known observations
+    sigma = 1  # Standard deviation of the Gaussian likelihood
+    func = (1/np.sqrt(6.28*sigma**2))*np.exp((obs_y-theta1-theta2*obs_x)**2/(-2*sigma**2))
+    return func
 
-# define theta space
-theta1 = np.arange(-5.0,5.1,0.1)
-theta2 = np.arange(-5.0,5.1,0.1)
-theta2 = theta2[::-1]  # minimum value from lowerleft corner
-T1,T2 = meshgrid(theta1, theta2) # grid of point
+# True linear function
+def linearF(x):
+    func = 3 + 2*x
+    return func
 
-# true linear function
-obs_x = np.random.rand(3)[:,None]; obs_x = obs_x*7-4  # draw 3 observations in [0,3]
-true_x = np.arange(-5.0,5.1,0.1)
-obs_y = 3 + 2*obs_x + np.random.randn(len(obs_x),1)*0.5
-true_y = 3 + 2*true_x
-obs = np.hstack((obs_x, obs_y))
+# Function for visualizing observations and likelihood
+def plotLikelihood(obs_x, obs_y, k, likelihood):
+    # Observations
+    plt.subplot(1,2,1)
+    plt.plot(obs_x,obs_y,'k.',obs_x[:k+1],obs_y[:k+1],'ro')
+    plt.plot(true_x,true_y,'g--')
+    plt.xlim([-5.,5.])
+    plt.ylim([-7.5,12.5])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    # Likelihood
+    plt.subplot(1,2,2)
+    plt.imshow(likelihood,cmap='YlOrRd',extent=[-5,5,-5,5])
+    plt.xlabel('θ1')
+    plt.ylabel('θ2')
+
+    
+
+# Ground truth and noisy observations
+obs_x = np.random.rand(3)[:,None]; obs_x = obs_x*8-4  # Draw 3 noisy observations in [-4,4]
+obs_y = linearF(obs_x) + np.random.randn(len(obs_x),1)*1.0
+
+true_x = np.arange(-5.0,5.1,0.1)  # Ground truth data showing the linear function
+true_y = linearF(true_x)
+
+# Visualize ground truth data and noisy observation
 plt.plot(obs_x,obs_y,'ko',true_x,true_y,'g--')
 plt.legend(['observed','true'], loc=0)
 plt.xlim([-5.,5.])
@@ -41,24 +80,29 @@ plt.ylim([-7.5,12.5])
 plt.xlabel('x')
 plt.ylabel('y')
 
-# loop over number of observations
-posterior = 1
-for k in range(3):  #(len(obs)):
-    prior = pri(T1,T2) 
-    likelihood = likeli(T1, T2, obs[k,1], obs[k,0]) # evaluation of the function on the grid
-    posterior *= likelihood  #*prior
-    #plt.imshow(prior,cmap='coolwarm',extent=[-5,5,-5,5])
-plt.imshow(posterior,cmap='YlOrRd',extent=[-5,5,-5,5])
 
-# Plot 2D
-im = imshow(posterior,cmap='YlOrRd') # drawing the function
-# adding the Contour lines with labels
-#cset = contour(Z,np.arange(-1,1.5,0.2),linewidths=2,cmap=cm.Set2)
-#clabel(cset,inline=True,fmt='%1.1f',fontsize=10)
-colorbar(im) # adding the colobar on the right
-# latex fashion title
-#title('$z=(1-x^2+y^3) e^{-(x^2+y^2)/2}$')
-show()
+# Define theta space for visualizing likelihood and prior
+theta1 = np.arange(-5.0,5.1,0.1)
+theta2 = np.arange(-5.0,5.1,0.1)
+theta2 = theta2[::-1]  # minimum value from lowerleft corner
+T1,T2 = meshgrid(theta1, theta2) # grid of point
+
+# Loop over number of observations
+likelihood = 1
+for k in range(3):  #(len(obs_y)):
+    prior = pri(T1,T2) 
+    likelihood *= likeli(T1, T2, obs_y[k], obs_x[k]) # evaluation of the function on the grid
+    posterior = prior*likelihood
+
+
+# Visualization in 2D
+#im = imshow(posterior,cmap='YlOrRd') # drawing the function
+#colorbar(im) # adding the colobar on the right
+#show()
+plotLikelihood(obs_x, obs_y, k, likelihood)
+plt.imshow(prior,cmap='GnBu',extent=[-5,5,-5,5])
+plt.imshow(likelihood,cmap='YlOrRd',extent=[-5,5,-5,5])
+
 
 # Plot 3D
 fig = plt.figure()
